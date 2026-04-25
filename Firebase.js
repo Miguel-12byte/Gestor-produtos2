a49ad9c5c620b1ef80ad0169882ba9d2abb06d86
Miguel-12byte <barbat.12miguel@gmail.com>
2026-04-24 21:10:10 -0300

    Set up Firebase configuration and exports
    Initialize Firebase app with configuration and export necessary functions.

---
+++ Firebase.js
@@ -0,0 +1,26 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  query,
+  where
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "SUA_KEY",
+  authDomain: "SEU_DOMINIO",
+  projectId: "SEU_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export { signInWithEmailAndPassword, collection, addDoc, getDocs, query, where };
